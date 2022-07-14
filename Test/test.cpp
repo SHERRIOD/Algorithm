62053cd4656e8c0042f5b46e303aa4ed31c2770b
Sherriod <2012414014@qq.com>
2022-07-14 17:13:51 +0800

    成功了

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
+    cout<<"Please text a word"<<endl;
+     string tmp_s;
+    cin>>tmp_s;
     cout<<"Hello World"<<endl;
+    for(int i=0;i<5;i++){
+        cout<<"type "<<i<<endl;
+        cin>>tmp_s;
+        cout<<tmp_s;
+    }
 }

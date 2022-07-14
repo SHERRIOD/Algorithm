48a6417ae7221cb36299d9a5672d1d382d58ef61
Sherriod <2012414014@qq.com>
2022-07-14 17:28:42 +0800

    hihi

---
+++ Test2/test2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"type something to test the vscode"<<endl;
+    string tmp_s;
+    cin>>tmp_s;
+    cout<<"you type "<<tmp_s<<endl;
+}
